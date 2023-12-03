28901ea9d2133f300a8a23a56c3e012d8dc60e5f
Albeiro Marin <xlandru@hotmail.com>
2023-12-03 15:59:45 -0500

    Evita que los ciclos de vida se ejecuten doble

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'm.media-amazon.com',
-            port: '',
-            pathname: '/images/M/**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        port: "",
+        pathname: "/images/M/**",
       },
-}
+    ],
+  },
+  reactStrictMode: false
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
