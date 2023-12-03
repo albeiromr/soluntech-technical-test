1cc09244fdb8b0fffa56281a19713a2151a6d118
Albeiro Marin <xlandru@hotmail.com>
2023-12-03 07:42:17 -0500

    Agrega seats explanation

--- src/app/select-seat/page.tsx
+++ src/app/select-seat/page.tsx
@@ -1,4 +1,5 @@
 import SeatsComponent from "@/components/seats.component";
+import Image from "next/image";
 
 export default function SelectSeatPage() {
   return (
@@ -9,6 +10,42 @@ export default function SelectSeatPage() {
 
       <SeatsComponent />
       
+      <div className="page__seats-explanation">
+
+        <span>
+          <Image
+            src="/assets/selected-seat.png"   
+            alt="seat image"
+            width={97}
+            height={67}
+            priority={true}
+          />
+          <p>Selected</p>
+        </span>
+
+        <span>
+          <Image
+            src="/assets/not-available-seat.png"   
+            alt="seat image"
+            width={97}
+            height={67}
+            priority={true}
+          />
+          <p>Booked</p>
+        </span>
+
+        <span>
+          <Image
+            src="/assets/available-seat.png"   
+            alt="seat image"
+            width={97}
+            height={67}
+            priority={true}
+          />
+          <p>Available</p>
+        </span>
+
+      </div>
     </section>
   )
 }
